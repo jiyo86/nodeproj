6ea48cd1c3041470f6c2301ec12eb8edfecdbfe0
JIYO MATHEW <43206989+jiyo86@users.noreply.github.com>
2022-03-15 22:54:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const cookie_parser = require("cookie-parser");
+const cors = require("cors");
+const users = require("./routes/users");
+
 const app = express();
-exports.module = app;
-app.get("/", function(req, resp, next) {
-  resp.send("Welcome");
+app.use(cors());
+app.use(cookie_parser());
+app.use(express.json());
+app.use("/", users);
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  res.status(404).json("Endpoint not found");
 });
 
-app.listen(8080, function(){console.log("listen")})
+var port = process.env.PORT || "8080";
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
